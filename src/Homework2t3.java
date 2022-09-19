56418f22b1f211b75c0ad57bc2e80f035287fae0
xenia <ksen.novikova@mail.ru>
2022-09-19 23:20:26 +0300

    добавлен цикл

---
+++ src/Homework2t3.java
@@ -0,0 +1,13 @@
+public class Homework2t3{
+    public static void main(String[] args) {
+        var frog=3.5;
+        System.out.println(frog);
+        frog=frog*10;
+        System.out.println(frog);
+        frog=frog/3.5;
+        System.out.println(frog);
+        frog=frog+4;
+        System.out.println(frog);
+
+    }
+}
---
+++ src/Homework2t678.java
@@ -0,0 +1,19 @@
+public class Homework2t678 {
+    public static void main(String[] args) {
+        var firstWaight=78.2;
+                var secondWaight=82.7;
+                var generalWeight=firstWaight+secondWaight;
+                System.out.println(generalWeight);
+             var differentWeight=secondWaight-firstWaight;
+        System.out.println(differentWeight);
+        differentWeight=secondWaight%firstWaight;
+        System.out.println(differentWeight);
+ var totalHours=640;
+ var everyEmployerHours=8;
+ var totalEmployers=totalHours/everyEmployerHours;
+ System.out.println(totalEmployers);
+ totalEmployers=totalEmployers+64;
+ everyEmployerHours=totalHours/totalEmployers;
+ System.out.println(everyEmployerHours);
+    }
+}
